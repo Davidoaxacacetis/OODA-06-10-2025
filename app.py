7aecb8745fa6ccc58c548d6f9160e863aa40c95a
= <=>
2025-10-07 09:08:25 -0500

    fsa

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask (__name__)
 
 @app.route("/")
 def index():
-    miembros =  ["Ramon Renteria","David Oaxaca","Jesús Nolasco"]
-    return render_template ("index.html", creador="Oaxaca Orona David Adrián", nombres = miembros)
+    return render_template ("index1.html")
 
 if __name__ =='__main__':
     app.run(debug=True)
