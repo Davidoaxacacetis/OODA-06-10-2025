820803ed1553a35b5416156c71b7bb258790daf4
= <=>
2025-10-08 09:13:43 -0500

    fsd

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask (__name__)
 def index():
     return render_template ("index1.html")
 
+@app.route("/tiempo-nurburgring")
+def tiempo():
+    return render_template ("autos.html")
+
+@app.route("/maravillas-del-mundo")
+def maravillas():
+    return render_template ("index1.html")
+
+@app.route("/animales-exoticos")
+def animales():
+    return render_template ("index1.html")
+
+@app.route("/acerca-de")
+def acecade():
+    return render_template ("index1.html")
+
 if __name__ =='__main__':
     app.run(debug=True)
