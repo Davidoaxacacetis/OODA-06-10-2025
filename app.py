e9defba55c577f0717ac0abf34a1a1aaa24d0959
= <=>
2025-10-15 09:09:34 -0500

    jlk

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask (__name__) 
 
@@ -21,6 +21,14 @@ def animales():
 @app.route("/acerca-de")
 def acecade():
     return render_template ("acercade.html")
+
+@app.route("/registrate")
+def registro():
+    return render_template ("registro.html")
+
+@app.route("/inicia-sesion")
+def sesion():
+    return render_template ("inicia.html")
 
 if __name__ =='__main__':
     app.run(debug=True)
