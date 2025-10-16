2306291550b66e38c7f94d8c4edc8692334eaaba
= <=>
2025-10-16 08:26:58 -0500

    b.jhk

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask (__name__) 
+
+app.config['SECRET_KEY']="OODAAXRTBTOPSOPM"
 
 @app.route("/")
 def index():
@@ -25,6 +27,27 @@ def acecade():
 @app.route("/registrate")
 def registro():
     return render_template ("registro.html")
+
+@app.route("/registrame", method =("GET","POST"))
+def registrame():
+    error = None
+    if request.method == "POST":
+        nombrecompleto = request.form["name"]
+        apellido = request.form["apellido"]
+        email = request.form["email"]
+        contraseña = request.form["Contraseña"]
+        confirmarcontra = request.form["confirmarcontra"]
+        fecha_de_nacimiento = request.form["Dia"]
+        gener = request.form["genero"]
+        
+        if contraseña != confirmarcontra:
+            error = "La contraseña no coincide"
+        if error != None:
+            flash(error)
+            return  render_template("registro.html")
+        else:
+            flash(f"¡Resgistro exitoso para el usuario:{nombrecompleto}!")
+            return render_template("index1.html")
 
 @app.route("/inicia-sesion")
 def sesion():
